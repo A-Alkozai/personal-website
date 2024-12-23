8af9bf467f0b1f3f45513714866d136acd3eff65
Alkozai Ahad (2023) ZMAC392 <ahad.alkozai.2023@live.rhul.ac.uk>
2024-12-23 14:24:15 +0000

    added button not available functionality

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+// When user tries to visit the website they are on
 const funText = ["{-_-}", "//\\(oo)/\\\\", "¯\\_(ツ)_/¯", "┻━┻︵ \\(°□°)/ ︵ ┻━┻ ", "(☞ﾟ∀ﾟ)☞", "(ง •̀_•́)ง", "ಠ_ಠ", "(╭ರ_•́)"];
+const websiteLink = document.getElementById("this-website-link");
+const websiteLinkFooter = document.getElementById("this-website-footer");
+
+websiteLinkFooter.addEventListener("click", myWebsiteLink);
 
 function myWebsiteLink() {
-    let index = Math.floor(Math.random() * funText.length);
-    document.getElementById("this-website-link").innerHTML = funText[index];
-    document.getElementById("this-website-footer").onclick = inverseMyWebsiteLink; 
+    let innerHTML = websiteLink.innerHTML;
+
+    if (innerHTML == "Visit Website") {
+        let index = Math.floor(Math.random() * funText.length);
+        websiteLink.innerHTML = funText[index];
+        websiteLinkFooter.onclick = inverseMyWebsiteLink; 
+    } 
+    else {
+        websiteLink.innerHTML = "Visit Website"
+    }
 }    
 
-function inverseMyWebsiteLink() {
-    document.getElementById("this-website-link").innerHTML = "Visit Website";
-    document.getElementById("this-website-footer").onclick = myWebsiteLink; 
+// Unavailable button
+var unavailableButtons = document.querySelectorAll(".unavailable");
+
+for (i = 0; i < unavailableButtons.length; i++) {
+    unavailableButtons[i].addEventListener("click", unavailableButton);
+}
+
+function unavailableButton(event) {
+    var target = event.currentTarget;
+    var innerHTML = target.innerHTML;
+
+    target.innerHTML = "Unavailable :C";
+    target.removeEventListener("click", unavailableButton);
+    setTimeout(revertUnavailableButton, 1000, event.currentTarget, innerHTML);
+}
+
+function revertUnavailableButton(target, original) {
+    target.innerHTML = original;
+    target.addEventListener("click", unavailableButton);
 }
